2ea4529db517901a6cf4427b8cbef98ed449533d
Haris Sekar <sharis14003@gmail.com>
2021-12-27 17:29:40 +0530

    database connection changed

--- conn.php
+++ conn.php
@@ -1,11 +1,34 @@
 <?php
-    $conn=mysqli_connect("localhost","root","","blood");
-    if(mysqli_connect_error())
-    {
-        echo "cannot connect DB";
-    }
-    else{
-       #echo "s";
-    }
-    
+
+//clever cloud DB connect Details
+
+$host='bkh0w9hocxm40oui3tar-mysql.services.clever-cloud.com:3306';
+$username='uxrhj9beyinutnxh';
+$password='PV0NOkhpF4VRYljdKSea';
+$database='bkh0w9hocxm40oui3tar';
+
+//infinty free DB connect Details
+
+// $host='sql111.epizy.com:3306';
+// $username='epiz_28492601';
+// $password='M351OeVmZO';
+// $database='epiz_28492601_avs_enter_online';
+
+// localhost DB connect Details
+
+// $host='localhost';
+// $username='root';
+// $password='';
+// $database='blood';
+
+
+
+$conn=mysqli_connect($host,$username,$password,$database);
+if(mysqli_connect_error())
+{   
+    echo "cannot connect DB";
+}
+else{
+    // echo "sus";
+}
 ?>
